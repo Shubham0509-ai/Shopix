42760d94a111d37ea123598f87dbaa5d0fab283e
Shubham0509-ai <shubhamsharmaxyz12@gmail.com>
2026-08-30 13:01:25 +0530

    Added package.json with initial dependencies and scripts

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+
+
+export const signup = async (req, res) => {
+    res.send("Signup end point");
+};
+
+export const login = async (req, res) => {
+    res.send("Login end point");
+};
+
+export const logout = async (req, res) => {
+    res.send("Logout end point");
+};

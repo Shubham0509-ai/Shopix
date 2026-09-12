62266c95bdeb6f55e6cbd03d0cfd0aa0321b2302
Shubham0509-ai <shubhamsharmaxyz12@gmail.com>
2026-09-12 11:47:02 +0530

    Implemented all pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,35 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import './index.css'
-import App from './App.jsx'
-import HomePage from './pages/HomePage.jsx'
-import SignupPage from './pages/SignupPage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />
-      },
-      {
-        path: "/signup",
-        element: <SignupPage />
-      },
-      {
-        path: "/login",
-        element: <LoginPage />
-      }
-    ]
-  }
-]);
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
+);
